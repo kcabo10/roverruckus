dc30623c8a70cc57873878c4df6bfda2338adcf6
seansankey <seansankey@cox.net>
2018-10-04 20:27:10 -0700

    adding HardwarePushBot class and calling robot.init method to initialize hardware map. adding test class to verify nullpointerexception in MichaelGyroTurnTest.class. adding same to 3 more classes + javadoc for explanation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/MichaelGyroTurnTest_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/MichaelGyroTurnTest_Test.java
@@ -8,37 +8,35 @@ import org.firstinspires.ftc.teamcode.MichaelRandomTesting;
 public class MichaelGyroTurnTest_Test {
 
     /**
-     *
-     If you get a NullPointerException originating in the runOpMode() method like this:
-
-     java.lang.NullPointerException
-     at org.firstinspires.ftc.teamcode.MichaelGyroTurnTest2.runOpMode(MichaelGyroTurnTest2.java:34)
-
-     then you need to initialize the robot hardware in the class by
-     (1) adding/instantiating the HardwarePushBot class to your class and
-     (2) calling the init method on the HardwarePushBot class like this:
-
-     HardwarePushbot robot   = new HardwarePushbot();
-     robot.init(hardwareMap);
-
-     (3) Add the robot hardware configuration to the phone (left_drive, right_drive, etc.).
-
-
-
-     For the following 4 methods, if you get a NullPointerException like the one below:
-
-     java.lang.NullPointerException
-     at org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot.init(HardwarePushbot.java:81)
-
-     This is correct.
-
-     The NullPointerException in this case is due to the hardware on the robot not
-     connected to this test class.  When you connect the robot to the phone and configure the
-     hardware (motors) on the phone, your program should run correctly.
-
-
-     Sean
-
+     * If you get a NullPointerException originating in the runOpMode() method like this:
+     * <p>
+     * java.lang.NullPointerException
+     * at org.firstinspires.ftc.teamcode.MichaelGyroTurnTest2.runOpMode(MichaelGyroTurnTest2.java:34)
+     * <p>
+     * then you need to initialize the robot hardware in the class by
+     * (1) adding/instantiating the HardwarePushBot class to your class and
+     * (2) calling the init method on the HardwarePushBot class like this:
+     * <p>
+     * HardwarePushbot robot   = new HardwarePushbot();
+     * robot.init(hardwareMap);
+     * <p>
+     * (3) Add the robot hardware configuration to the phone (left_drive, right_drive, etc.).
+     * <p>
+     * <p>
+     * <p>
+     * For the following 4 methods, if you get a NullPointerException like the one below:
+     * <p>
+     * java.lang.NullPointerException
+     * at org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot.init(HardwarePushbot.java:81)
+     * <p>
+     * This is correct.
+     * <p>
+     * The NullPointerException in this case is due to the hardware on the robot not
+     * connected to this test class.  When you connect the robot to the phone and configure the
+     * hardware (motors) on the phone, your program should run correctly.
+     * <p>
+     * <p>
+     * Sean
      */
 
     MichaelGyroTurnTest michaelGyroTurnTest = new MichaelGyroTurnTest();
@@ -59,7 +57,7 @@ public class MichaelGyroTurnTest_Test {
 
     }
 
-     private void run(String[] args) throws Exception {
+    private void run(String[] args) throws Exception {
 
         michaelGyroTurnTest();
         michaelGyroTurnTest2();
