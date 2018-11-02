bc5041a782b0a8eeddaeec936484cd5bc860734f
katie <kcabo10@gmail.com>
2018-11-01 19:37:32 -0700

    The mineral detection is working correctly now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasterAutonomousProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasterAutonomousProgram.java
@@ -51,9 +51,7 @@ public class MasterAutonomousProgram extends LinearOpMode {
 
         waitForStart();
 
-        /**
-         landing our robot
-        */
+        // landing our robot
 
 //        robot.lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 //        robot.lift.setTargetPosition(-12500);
@@ -69,21 +67,9 @@ public class MasterAutonomousProgram extends LinearOpMode {
 //
 //        robot.lift.setPower(0);
 
-        /**
-        Setting code position
-         */
-
         int codePos = 0;
 
-        /**
-         Getting the mineral position
-         */
-
         getMineralPosition();
-
-        /**
-         Printing telemetry
-         */
 
         printTelemetry(10);
 
@@ -92,36 +78,18 @@ public class MasterAutonomousProgram extends LinearOpMode {
         telemetry.addData("Turning", telemetry);
         telemetry.update();
 
-        /**
-         Turning robot 45 degrees and driving forward 800 encoder ticks
-         */
-
         gyroTurn.turnGyro(45);
-        gyroDrive.driveGyro(.3, -800);//1611);
-
-        /**
-         Printing telemetry
-         */
+        gyroDrive.driveGyro(.3, -400);//1611);
 
         printTelemetry(20);
 
-        /**
-         Reading vuMark
-         */
-
         foundTargetName = vuforia.getTarget();
-
-        /**
-         Getting grid position from vuMark
-         */
         gridNavigation.setGridPosition(vuforia.gridPos[0], vuforia.gridPos[1], vuforia.rotation.thirdAngle);
-
-        /**
-         Printing telemetry
-         */
 
         telemetry.addData("Grid Nav Set Pos X", vuforia.gridPos[0]);
         telemetry.addData("Grid Nav Set Pos Y", vuforia.gridPos[1]);
+        telemetry.update();
+        sleep(3000);
 
         printTelemetry(30);
 
@@ -130,36 +98,31 @@ public class MasterAutonomousProgram extends LinearOpMode {
 
         sleep(3000);
 
-        /**
-         Setting x and y
-          */
-
         int X = 0;
         int Y = 1;
 
-        /**
-         Change values to grab mineral
-         */
+       /**
+        Change values to grab mineral
+        */
 
-        double[] BLUE_DEPOT_LEFT = {-2, 1};
-        double[] BLUE_DEPOT_RIGHT = {-1, 2};
-        double[] BLUE_DEPOT_CENTER = {-1.5, 1.5};
 
-        double[] BLUE_CRATER_LEFT = {1, 2};
-        double[] BLUE_CRATER_RIGHT = {2, 1};
-        double[] BLUE_CRATER_CENTER = {1.5, 1.5};
+        double[] BLUE_DEPOT_LEFT = {-2, -1};
+        double[] BLUE_DEPOT_RIGHT = {-1, -2};
+        double[] BLUE_DEPOT_CENTER = {-1.5, -1.5};
 
-        double[] RED_DEPOT_LEFT = {2, -1};
-        double[] RED_DEPOT_RIGHT = {1, -2};
-        double[] RED_DEPOT_CENTER = {1.5, -1.5};
+        double[] BLUE_CRATER_LEFT = {1, -2};
+        double[] BLUE_CRATER_RIGHT = {2, -1};
+        double[] BLUE_CRATER_CENTER = {1.5, -1.5};
 
-        double[] RED_CRATER_LEFT = {-1, -2};
-        double[] RED_CRATER_RIGHT = {-2, -1};
-        double[] RED_CRATER_CENTER = {-1.5, -1.5};
+        double[] RED_DEPOT_LEFT = {2, 1};
+        double[] RED_DEPOT_RIGHT = {1, 2};
+        double[] RED_DEPOT_CENTER = {1.5, 1.5};
 
-        /**
-        Change values to park
-         */
+        double[] RED_CRATER_LEFT = {-1, 2};
+        double[] RED_CRATER_RIGHT = {-2, 1};
+        double[] RED_CRATER_CENTER = {-1.5, 1.5};
+
+//        Change values to park
 
         double[] BLUE_DEPOT_LEFT_PARKING = {0, 0};
         double[] BLUE_DEPOT_RIGHT_PARKING = {0, 0};
@@ -177,33 +140,20 @@ public class MasterAutonomousProgram extends LinearOpMode {
         double[] RED_CRATER_RIGHT_PARKING = {0, 0};
         double[] RED_CRATER_CENTER_PARKING = {0, 0};
 
-        /**
-         Determining target position and following set commands
-         */
-
         switch (foundTargetName) {
             case "Red-Footprint":
                 telemetry.addData("Telemetry", "Red Crater Program");
                 printTelemetry(40);
 
                 if (goldPosition == "LEFT") {
-                    /**
-                     Drives to the red crater left position using preset grid positions
-                     */
                     gridNavigation.driveToPosition(RED_CRATER_LEFT[X], RED_CRATER_LEFT[Y], .2);
 //                    gridNavigation.driveToPosition(RED_CRATER_LEFT_PARKING[X], RED_CRATER_LEFT_PARKING[Y], .2);
                 }
                 if (goldPosition == "RIGHT"){
-                    /**
-                     Drives to the red crater right position using preset grid positions
-                     */
                     gridNavigation.driveToPosition(RED_CRATER_RIGHT[X], RED_CRATER_RIGHT[Y], .2);
 //                    gridNavigation.driveToPosition(RED_CRATER_RIGHT_PARKING[X], RED_CRATER_RIGHT_PARKING[Y], .2);
                 }
                 if (goldPosition == "CENTER"){
-                    /**
-                     Drives to the red crater center position using preset grid positions
-                     */
                     gridNavigation.driveToPosition(RED_CRATER_CENTER[X], RED_CRATER_CENTER[Y], .2);
 //                    gridNavigation.driveToPosition(RED_CRATER_CENTER_PARKING[X], RED_CRATER_CENTER_PARKING[Y], .2);
                 }
@@ -303,9 +253,6 @@ public class MasterAutonomousProgram extends LinearOpMode {
 
     }
 
-    /**
-     Print telemetry function
-     */
 
     private void printTelemetry(int codePos) {
         telemetry.addData("Target Found", foundTargetName);
@@ -314,24 +261,10 @@ public class MasterAutonomousProgram extends LinearOpMode {
         telemetry.update();
     }
 
-    /**
-     Get mineral position function
-     */
 
     public void getMineralPosition() {
-
-        /**
-         Debounce algoritm to detect mineral accurately
-         */
-
         int debounceCount = 0;
         long startTime = 0;
-        startTime = System.currentTimeMillis();
-
-        /**
-         Initializing dogeCV for getting mineral position
-         */
-
         String previousPosition;
         telemetry.addData("Status", "DogeCV 2018.0 - Sampling Order Example");
 
@@ -351,26 +284,27 @@ public class MasterAutonomousProgram extends LinearOpMode {
 
         detector.enable();
 
+
+
         goldPosition = "UNKNOWN";
         previousPosition = "UNKNOWN";
         telemetry.addData("MAP", "Running Sampling Order loop");
-        goldPosition = detector.getCurrentOrder().toString();
+        goldPosition = detector.getLastOrder().toString();
 
-        /**
-         This while function is waiting until it has seen the correct mineral position by finding the value of one of the mineral position then debouncing the mineral position until it settles at the correct mineral position.
-         */
         while (goldPosition == "UNKNOWN") {
 
-            gyroTurn.turnGyro(-5);
+            // waits to turn after it has tried to check for the mineral pos
+
+            if (startTime != 0) gyroTurn.turnGyro(-5);
 
             sleep(5000);
 
             startTime = System.currentTimeMillis();
 
 
-            while (System.currentTimeMillis() < (startTime + 10000)) {
+            while (System.currentTimeMillis() < (startTime + 3000)) {
 
-                goldPosition = detector.getCurrentOrder().toString();
+                goldPosition = detector.getLastOrder().toString();
 
                 if (goldPosition == previousPosition) {
                 }
@@ -390,9 +324,6 @@ public class MasterAutonomousProgram extends LinearOpMode {
             telemetry.addData("Current Order", detector.getCurrentOrder().toString()); // The current result for the frame
             telemetry.addData("Last Order", detector.getLastOrder().toString()); // The last known result
 
-            /**
-             Telemetry for gold position
-             */
 
             switch (goldPosition) {
                 case ("LEFT"):
