f1f8aff7fcfb57ae00497731b2dcda05c9a52f9f
Kylie <kylie1234@cox.net>
2018-11-07 14:01:59 -0700

    Testing CRServo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBeep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBeep.java
@@ -19,10 +19,11 @@ public class HardwareBeep {
     public DcMotor rightFront = null;
     public DcMotor rightBack = null;
     public DcMotor lift = null;
-//    public DcMotor arm = null;
-//    public DcMotor armExtrusion = null;
-//    public DcMotor intake = null;
+    public DcMotor arm = null;
+    public DcMotor armExtrusion = null;
+    public DcMotor intake = null;
 //    public Servo basket = null;
+    public Servo latch = null;
 
     public BNO055IMU imu = null;
 
@@ -47,19 +48,20 @@ public class HardwareBeep {
         rightFront = hwMap.get(DcMotor.class, "right_front");
         rightBack = hwMap.get(DcMotor.class, "right_back");
         lift = hwMap.get(DcMotor.class, "lift");
-//        arm = hwMap.get(DcMotor.class, "arm");
-//        armExtrusion = hwMap.get(DcMotor.class, "arm_extrusion");
-//        intake = hwMap.get(DcMotor.class, "intake");
+        arm = hwMap.get(DcMotor.class, "arm");
+        armExtrusion = hwMap.get(DcMotor.class, "arm_extrusion");
+        intake = hwMap.get(DcMotor.class, "intake");
 //        basket = hwMap.get(Servo.class, "basket");
+        latch = hwMap.get(Servo.class, "latch");
 
         leftFront.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         leftBack.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightFront.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
         rightBack.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
         lift.setDirection(DcMotor.Direction.FORWARD);
-//        arm.setDirection(DcMotor.Direction.FORWARD);
-//        armExtrusion.setDirection(DcMotor.Direction.FORWARD);
-//        intake.setDirection(DcMotor.Direction.FORWARD);
+        arm.setDirection(DcMotor.Direction.FORWARD);
+        armExtrusion.setDirection(DcMotor.Direction.FORWARD);
+        intake.setDirection(DcMotor.Direction.FORWARD);
 
 
         // Set all motors to zero power
@@ -68,10 +70,11 @@ public class HardwareBeep {
         rightFront.setPower(0);
         rightBack.setPower(0);
         lift.setPower(0);
-//        arm.setPower(0);
-//        armExtrusion.setPower(0);
-//        intake.setPower(0);
-//        basket.setPosition(0.50);
+        arm.setPower(0);
+        armExtrusion.setPower(0);
+        intake.setPower(0);
+//        basket.setPosition(0.45);
+        latch.setPosition(.45);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
@@ -80,8 +83,8 @@ public class HardwareBeep {
         rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpProgram.java
@@ -41,40 +41,47 @@ public class TeleOpProgram extends OpMode
     }
 
     public void init_loop() {
+
         buttonYPressed = 0;
         robot.lift.setPower(0);
-//        robot.arm.setPower(0);
-//        robot.armExtrusion.setPower(0);
-//        robot.intake.setPower(0);
-//        robot.basket.setPosition(0);
+        robot.arm.setPower(0);
+        robot.armExtrusion.setPower(0);
+        robot.intake.setPower(0);
+//        robot.basket.setPosition(.45);
+        robot.latch.setPosition(.45);
     }
 
     public void loop() {
+        double left;
+        double right;
+        double drive;
+        double turn;
+        double max;
 
         /**
          *POV Mecanum Wheel Control
          */
+        drive = -gamepad1.left_stick_y;
+        turn  =  gamepad1.right_stick_x;
 
-        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-        double rightX = gamepad1.right_stick_x;
-        final double v1 = (((r * Math.cos(robotAngle) - rightX) * direction) * scaleFactor);
-        final double v2 = (((r * Math.sin(robotAngle) + rightX) * direction) * scaleFactor);
-        final double v3 = (((r * Math.sin(robotAngle) - rightX) * direction) * scaleFactor);
-        final double v4 = (((r * Math.cos(robotAngle) + rightX) * direction) * scaleFactor);
+        left  = drive + turn;
+        right = drive - turn;
 
-        robot.leftFront.setPower(v1);
-        robot.rightFront.setPower(v2);
-        robot.leftBack.setPower(v3);
-        robot.rightBack.setPower(v4);
+        max = Math.max(Math.abs(left), Math.abs(right));
+        if (max > 1.0)
+        {
+            left /= max;
+            right /= max;
+        }
 
-        telemetry.addData("v1", v1);
-        telemetry.addData("v2", v2);
-        telemetry.addData("v3", v3);
-        telemetry.addData("v4", v4);
-        telemetry.addData("direction", direction);
-        telemetry.addData("scaleFactor", scaleFactor);
-        telemetry.update();
+        left = left * scaleFactor * direction;
+
+        right = right * scaleFactor * direction;
+
+        robot.leftFront.setPower(left);
+        robot.leftBack.setPower(left);
+        robot.rightFront.setPower(right);
+        robot.rightBack.setPower(right);
 
         /**
          *Invert Direction On Y Button
@@ -116,64 +123,79 @@ public class TeleOpProgram extends OpMode
         /**
          *Lift Control
          */
+        if (gamepad2.dpad_down && !gamepad2.dpad_up)        {
 
-        if (gamepad2.left_bumper) {
             robot.lift.setPower(1);
-        } else if (gamepad2.left_trigger < 0) {
-            robot.lift.setPower(-1); }
+        } else if (gamepad2.dpad_up && !gamepad2.dpad_down) {
+            robot.lift.setPower(-1);
+        } else if (!gamepad2.dpad_up && !gamepad2.dpad_down) {
+            robot.lift.setPower(0);
+        }
+
+        /**
+         Latch Release Control
+          */
+
+        if (gamepad2.dpad_right && !gamepad2.dpad_left)        {
+
+            robot.latch.setPosition(1);
+        } else if (gamepad2.dpad_left && !gamepad2.dpad_right) {
+            robot.latch.setPosition(-1);
+        } else if (!gamepad2.dpad_left && !gamepad2.dpad_right) {
+            robot.latch.setPosition(0.45);
+        }
+
 
         /**
          *Intake Control
          */
-
-//        if (gamepad2.left_bumper) {
-//            robot.intake.setPower(0.3);
-//        } else if (gamepad2.left_trigger > 0) {
-//            robot.intake.setPower(-0.3);
-//        } else
-//            robot.intake.setPower(0);
+        if (gamepad2.right_bumper) {
+            robot.intake.setPower(0.3);
+        } else if (gamepad2.right_trigger > 0) {
+            robot.intake.setPower(-0.3);
+        } else
+            robot.intake.setPower(0);
 
         /**
          *Basket Control
          */
-//        if (gamepad2.dpad_down && !gamepad2.dpad_up)        {
-//
+//        if (gamepad2.left_bumper) {
 //            robot.basket.setPosition(1);
-//        } else if (gamepad2.dpad_up && !gamepad2.dpad_down) {
-//            robot.basket.setPosition(-1;
-//        } else if (!gamepad2.dpad_up && !gamepad2.dpad_down) {
-//            robot.basket.setPosition(0.45);
-//        }
+//        } else if (gamepad2.left_trigger > 0.45) {
+//            robot.basket.setPower(-1);
+//        } else
+//            robot.basket.setPower(0.45);
 
         /**
          *Arm Control
          */
 
-//        if (gamepad2.left_stick_y > 0) {
-//            robot.arm.setPower(0.3);
-//        } else if (gamepad2.left_stick_y < 0)
-//            robot.arm.setPower(-0.3);
-//        else if (gamepad2.left_stick_y == 0)
-//            robot.arm.setPower(0);
+        if (gamepad2.left_stick_y > 0) {
+            robot.arm.setPower(0.3);
+        } else if (gamepad2.left_stick_y < 0)
+            robot.arm.setPower(-0.3);
+        else if (gamepad2.left_stick_y == 0)
+            robot.arm.setPower(0);
 
         /**
         Arm Extrusions
          */
 
-//        if (gamepad2.right_stick_y > 0) {
-//            robot.armExtrusion.setPower(0.3);
-//        } else if (gamepad2.right_stick_y < 0)
-//            robot.armExtrusion.setPower(-0.3);
-//        else if (gamepad2.right_stick_y == 0)
-//            robot.armExtrusion.setPower(0);
+        if (gamepad2.right_stick_y > 0) {
+            robot.armExtrusion.setPower(0.3);
+        } else if (gamepad2.right_stick_y < 0)
+            robot.armExtrusion.setPower(-0.3);
+        else if (gamepad2.right_stick_y == 0)
+            robot.armExtrusion.setPower(0);
     }
     
     public void stop() {
 
         robot.lift.setPower(0);
-//        robot.arm.setPower(0);
-//        robot.armExtrusion.setPower(0);
-//        robot.intake.setPower(0);
-//        robot.basket.setPosition(0);
+        robot.arm.setPower(0);
+        robot.armExtrusion.setPower(0);
+        robot.intake.setPower(0);
+//        robot.basket.setPosition(.45);
+        robot.latch.setPosition(.45);
     }
 }
